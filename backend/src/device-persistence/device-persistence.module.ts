fc4e4c60f79ce51f6c314239d4b79bd9a46a9c2e
K. MOHHAMED ILFAN <mohhamedilfan@gmail.com>
2026-06-05 18:26:22 +0530

    Delete backend directory

--- backend/src/device-persistence/device-persistence.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DevicePersistenceService } from './device-persistence.service';
-
-@Module({
-  providers: [DevicePersistenceService],
-  exports: [DevicePersistenceService],
-})
-export class DevicePersistenceModule {}
--- backend/src/devices/dto/launch-device.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class LaunchDeviceDto {
-  @IsString()
-  deviceId!: string;
-
-  @IsString()
-  module!: string;
-}
